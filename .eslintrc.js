bf46beb6b69c5b5ef3526fcf4961eacc6b3dc776
Michio Nakagawa <Nakagawa_Michio@ogis-ri.co.jp>
2021-11-05 19:13:42 +0900

    build: setup env

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+    'jest/globals': true
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2019,
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.eslint.json'],
+  },
+  plugins: [
+    'jest',
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:jest/all',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'all',
+        endOfLine: 'lf',
+        semi: false,
+        singleQuote: true,
+        printWidth: 80,
+        tabWidth: 2,
+      },
+    ],
+  },
+}
+
