15bc9183ffbb8b7564d68f5d0a14cd05acec1e01
Michio Nakagawa <Nakagawa_Michio@ogis-ri.co.jp>
2021-11-15 11:11:56 +0900

    feat: add a subtraction function (#1)

---
+++ src/sub.ts
@@ -0,0 +1,8 @@
+/**
+ * Sub an integer number value.
+ *
+ * @param a The integer number value.
+ * @param b The integer number value.
+ * @returns The integer number value.
+ */
+export const sub = (a: number, b: number): number => a - b
